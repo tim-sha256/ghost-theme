3102fec3a6a726a9350781cf370411da2f6c67cc
tim-sha256 <tim.sha256@gmail.com>
2025-08-19 00:09:23 +0100

    ups razebal

--- assets/js/portal-customization.js
+++ assets/js/portal-customization.js
@@ -30,26 +30,10 @@ function customizePortal() {
             font-family: 'Iosevka Custom';
             src: url('/assets/fonts/IosevkaCustom-Regular.woff2') format('woff2');
         }
-        /* 
         * { 
             font-family: 'Iosevka Custom', monospace !important;
-            border-radius: 0 !important;
         }
-        */
         
-        /* Force square buttons with very high specificity 
-        button.gh-portal-btn,
-        .gh-portal-products-pricetoggle button,
-        .gh-portal-products-pricetoggle button[class*="gh-portal"],
-        .gh-portal-products-pricetoggle span[class*="gh-portal"],
-        [class*="gh-portal"][class*="button"],
-        [class*="gh-portal"][class*="btn"] {
-            border-radius: 0 !important;
-            -webkit-border-radius: 0 !important;
-            -moz-border-radius: 0 !important;
-        }
-        */
-       
         /* Make description look like link */
         .gh-portal-product-description {
             text-decoration: underline !important;
